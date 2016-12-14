e1d565e66d64c623ec87be3a5ee0cfd7f0eaa56e
SaberMeyt <SaberMeyt@gmail.com>
2016-12-14 15:38:28 +0300

    Заготовки для обмена: ошибка и класс для логина и пароля

---
+++ common/src/main/java/ru/mirea/common/VerificationException.java
@@ -0,0 +1,10 @@
+package ru.mirea.common;
+
+/**
+ * Created by Admin on 14.12.2016.
+ */
+public class VerificationException extends Exception {
+	public VerificationException(String  message) {
+		super(message);
+	}
+}
