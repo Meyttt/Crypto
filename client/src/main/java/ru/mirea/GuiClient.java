10f6483c092b81d707d19d82603c919b1d49a8ea
Saber <SaberMeyt@gmail.com>
2017-01-22 19:22:35 +0300

    Исправлена проблема с гуи

--- client/src/main/java/ru/mirea/GuiClient.java
+++ client/src/main/java/ru/mirea/GuiClient.java
@@ -26,11 +26,9 @@ public class GuiClient extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         this.stage=primaryStage;
-        // todo: добавить кнопку соединения
-        cryptoClient = new CryptoClient(new URI("http://localhost:8080")); // todo: настраиваемый параметр
+        cryptoClient = new CryptoClient(new URI("http://localhost:8080"));
         cryptoClient.exchangeBase();
         cryptoClient.exchangeKeys();
-
         GridPane grid = new GridPane();
         grid.setPadding(new Insets(5));
         grid.setHgap(5);
@@ -67,48 +65,25 @@ public class GuiClient extends Application {
             passwordInput.setStyle("-fx-text-fill: black;");}
         );
         reg.setOnAction(event -> {
-            try {
-                String answer = cryptoClient.registration(new VerificationData(loginInput.getText(),passwordInput.getText()));
-                switch (answer){
-                    case("New user added"):
-                        primaryStage.setScene(sceneSecond);
-                        output.setText(answer);
-                        break;
-                    case("Login already exists"):
-                        loginInput.setText("Login already exists");
-                        loginInput.setStyle("-fx-text-fill: red;");
-                        break;
-                }
+                    try {
+                        String answer = cryptoClient.registration(new VerificationData(loginInput.getText(), passwordInput.getText()));
+                        switch (answer) {
+                            case ("New user added"):
+                                primaryStage.setScene(sceneSecond);
+                                output.setText(answer);
+                                break;
+                            case ("Login already exists"):
+                                loginInput.setText("Login already exists");
+                                loginInput.setStyle("-fx-text-fill: red;");
+                                break;
+                        }
 
-            } catch (IOException ex) {
-//                output.setStyle("-fx-text-fill: red;");
-//                output.setText(ex.getMessage());
-        TextField output = new TextField();
-        output.setEditable(false);
-        Button ver = new Button("Вход");
-        Button reg = new Button("Регистрация");
-        reg.setOnAction(event -> {
-            try {
-                String answer = cryptoClient.registration(new VerificationData(loginInput.getText(),passwordInput.getText()));
-                output.setText(answer);
-            } catch (IOException ex) {
-                output.setStyle("-fx-text-fill: red;");
-                output.setText(ex.getMessage());
-            } catch (VerificationException e) {
-                e.printStackTrace();
-            }
-        });
-        ver.setOnAction(event -> {
-            try {
-                String answer = cryptoClient.verification(new VerificationData(loginInput.getText(),passwordInput.getText()));
-                output.setText(answer);
-            } catch (IOException ex) {
-                output.setStyle("-fx-text-fill: red;");
-                output.setText(ex.getMessage());
-            } catch (VerificationException e) {
-                e.printStackTrace();
-            }
-        });
+                    } catch (IOException ex) {
+                    } catch (VerificationException e) {
+                        e.printStackTrace();
+                    }
+                });
+
         ver.setOnAction(event -> {
             try {
                 String answer = cryptoClient.verification(new VerificationData(loginInput.getText(),passwordInput.getText()));
@@ -123,8 +98,7 @@ public class GuiClient extends Application {
                         passwordInput.setText("-fx-text-fill: red;");
                         loginInput.setText("-fx-text-fill: black;");
                 }
-//                primaryStage.setScene(sceneSecond);
-//                output.setText(answer);
+
             } catch (IOException ex) {
 //                output.setStyle("-fx-text-fill: red;");
 //                output.setText(ex.getMessage());
@@ -132,16 +106,8 @@ public class GuiClient extends Application {
                 e.printStackTrace();
             }
         });
-        grid.addRow(0, new Label("Логин:"), loginInput, reg);
-        grid.addRow(1, new Label("Пароль:"), passwordInput,ver);
-        grid.addRow(2,new Label("Ответ: "),output);
-        GridPane.setHgrow(loginInput, Priority.ALWAYS);
-        GridPane.setHgrow(passwordInput, Priority.ALWAYS);
-        Scene sceneFirst = new Scene(grid);
-        scene = sceneFirst;
-        primaryStage.setTitle("Обмен");
-        primaryStage.setScene(scene);
         primaryStage.show();
 
     }
-   }
+
+}
